ecd51ec9adacf79f8caa94418cdf54d359c0148f
KeerthikaReddyChalla <keerthikareddy2005@gmail.com>
2025-12-30 22:53:46 +0530

    fix errors in payment-service

--- backend/payment-service/src/main/java/com/chubb/payment/security/SecurityConfig.java
+++ backend/payment-service/src/main/java/com/chubb/payment/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
 @EnableMethodSecurity
@@ -18,5 +20,23 @@ public class SecurityConfig {
     public JwtDecoder jwtDecoder() {
         SecretKey key = new SecretKeySpec(SECRET.getBytes(), "HmacSHA256");
         return NimbusJwtDecoder.withSecretKey(key).build();
+    }
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+
+        JwtGrantedAuthoritiesConverter authoritiesConverter =
+                new JwtGrantedAuthoritiesConverter();
+
+
+        authoritiesConverter.setAuthoritiesClaimName("authorities");
+
+        authoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter converter =
+                new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return converter;
     }
 }
