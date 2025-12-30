bb4cab28c562796df117908c7aeb0de1e03ec890
KeerthikaReddyChalla <keerthikareddy2005@gmail.com>
2025-12-30 22:48:13 +0530

    add global exception handler in report-service

---
+++ backend/report-service/src/main/java/com/chubb/report/exception/ReportNotFoundException.java
@@ -0,0 +1,7 @@
+package com.chubb.report.exception;
+
+public class ReportNotFoundException extends RuntimeException {
+    public ReportNotFoundException(String message) {
+        super(message);
+    }
+}
