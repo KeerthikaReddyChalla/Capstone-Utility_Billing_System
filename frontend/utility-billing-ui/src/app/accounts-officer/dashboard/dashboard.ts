411382b3e4a6a1c8354cee1c0adfd8e8bd4c4d6b
KeerthikaReddyChalla <keerthikareddy2005@gmail.com>
2026-01-02 00:46:35 +0530

    add accounts-officer components in UI

---
+++ frontend/utility-billing-ui/src/app/accounts-officer/dashboard/dashboard.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../shared/components/navbar/navbar';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  templateUrl: './dashboard.html',
+  styleUrls: ['./dashboard.css'],
+  imports: [CommonModule, NavbarComponent] 
+})
+export class AccountsDashboardComponent {
+
+}
